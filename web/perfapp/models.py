38fb2ae1133b9b53944f7fa5dcdcb5db39e5dc7c
Ryan Saitta <rsaitta@mail.csuchico.edu>
2019-02-10 01:54:10 +0000

    Update web/perfapp/models.py
    Former-commit-id: 8124a9a5ee50952dcf1c78f3b2c0b0fc29353624

--- web/perfapp/models.py
+++ web/perfapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    #django provides an automatic id field
+
+class Task(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_stamp = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10)
+        class Meta:
+            ordering= ["owner","time_stamp"]
+
+    
