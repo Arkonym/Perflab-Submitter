b09b3b8c9d5d86d2a15b411a7151bc9cdc08f0ac
RSaitta <rsaitta@mail.csuchico.edu>
2019-04-06 00:02:41 -0700

    defined custom table for home page using django_tables2
    Former-commit-id: 76a56ea110f0a3f5c67009f7bb78e84e375b2457

---
+++ web/perfapp/tables.py
@@ -0,0 +1,14 @@
+import django_tables2 as tables
+from perfapp.models import Profile
+from django.contrib.auth.models import User
+
+
+
+class ScoreTable(tables.Table):
+    class Meta:
+        model = User
+        sequence = ['id', 'profile.max_score', 'profile.max_score_date', 'last_login']
+        fields = ['id', 'profile.max_score', 'profile.max_score_date', 'last_login']
+        exclude = ['password', 'email', 'first_name', 'last_name', 'is_staff',
+        'is_active', 'is_superuser', 'date_joined']
+        attrs= {'class': 'table'}
