dfe0f392be0a90735673328988f5768f3da5d4ed
RSaitta <rsaitta@mail.csuchico.edu>
2019-04-06 00:05:44 -0700

    updated urls to provide uid to view
    Former-commit-id: 592463ab1c2fa46194907d5db32bf18764c03be2

--- web/perfapp/urls.py
+++ web/perfapp/urls.py
@@ -6,8 +6,8 @@ from . import views
 
 urlpatterns = [
     path('home/', views.home, name='Home'),
-    path('profile/', views.profile, name='Profile'),
-    path('profile/update', views.update_profile),
+    path('profile/<int:user_id>', views.profile, name='Profile'),
+    path('profile/<int:user_id>/update', views.update_profile),
     path('submit/', views.submit, name='Submit'),
     path('login/', auth_views.LoginView.as_view()),
     path('logout/', views.logout_view),
