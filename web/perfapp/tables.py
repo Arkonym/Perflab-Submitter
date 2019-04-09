eb97ed4f13d4c7be6b460c762810669c185fb773
RSaitta <rsaitta@mail.csuchico.edu>
2019-04-08 23:09:43 -0700

    added tables for history and current jobs
    Former-commit-id: 97eead0902d8f9b5470e4f64a6d6ba851dffacdf

--- web/perfapp/tables.py
+++ web/perfapp/tables.py
@@ -1,6 +1,7 @@
 import django_tables2 as tables
-from perfapp.models import Profile
+from perfapp.models import Profile, Attempt, Job, Server
 from django.contrib.auth.models import User
+
 
 
 
@@ -12,3 +13,16 @@ class ScoreTable(tables.Table):
         exclude = ['password', 'email', 'first_name', 'last_name', 'is_staff',
         'is_active', 'is_superuser', 'date_joined']
         attrs= {'class': 'table'}
+
+class HistoryTable(tables.Table):
+    class Meta:
+        model = Attempt
+        exclude=['id']
+        attrs={'class':'table'}
+
+class JobTable(tables.Table):
+    class Meta:
+        model= Job
+        sequence = ['id', 'status', 'time_stamp']
+        fields= ['id', 'time_stamp', 'status']
+        exclude=['Filtermain', 'Filter_c', 'Filter_h', 'Makefile', 'cs1300_c', 'cs1300_h']
