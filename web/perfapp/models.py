7b19b45a5434a683248d56e96f8354c778bc8085
RSaitta <rsaitta@mail.csuchico.edu>
2019-04-12 02:04:21 -0700

    updated models for needed data.
    Former-commit-id: ff2971f526196674cf26f277ef66b9e4dfd35e02

--- web/perfapp/models.py
+++ web/perfapp/models.py
@@ -3,19 +3,22 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+
+import os
 # Create your models here.
 
 class Server(models.Model):
     ip = models.CharField(max_length=100)
     hostname = models.CharField(max_length=100, default="")
+    uID = models.SmallIntegerField(blank=True, default=-1)
     inUse = models.BooleanField(default=False)
+
 
     def __str__(self):
         return self.hostname + " " + self.ip + " " + str(self.inUse)
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    last_login =models.DateTimeField(auto_now_add=True, blank=True)
     max_score = models.DecimalField(blank=True, null=True, max_digits=4, decimal_places=2)
     max_score_date = models.DateTimeField(null=True, blank=True)
     #django provides an automatic id field
@@ -39,29 +42,22 @@ class Attempt(models.Model):
     def __str__(self):
         return self.time_stamp.strftime("%Y-%m-%d %H:%M:%S") + " (" + str(score)+ ")"
 
+# def get_upload_path(instance, jid, filename):
+#     return return '/code/uploads/{0}/{1}/{2}'
 
 class Job(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     task_id= models.SmallIntegerField(blank=True, default=-1)
     time_stamp = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10)
+    status = models.CharField(max_length=10, default="New")
     percent_complete = models.SmallIntegerField(blank=True, default=0)
-    FilterMain = models.FileField()
-    Filter_c = models.FileField()
-    Filter_h = models.FileField()
-    Makefile = models.FileField()
-    cs1300_c = models.FileField()
-    cs1300_h = models.FileField()
+    config = models.FileField(blank=True, null=True)
+    FilterMain = models.FileField(blank=True, null=True)
+    Filter_c = models.FileField(blank=True, null=True)
+    Filter_h = models.FileField(blank=True, null=True)
+    Makefile = models.FileField(blank=True, null=True)
+    cs1300_c = models.FileField(blank=True, null=True)
+    cs1300_h = models.FileField(blank=True, null=True)
 
     def __str__(self):
         return self.owner +  self.time_stamp.strftime("%Y-%m-%d %H:%M:%S")
-    # class Meta:
-    #     ordering= ["owner","time_stamp"]
-
-# class Upload(models.Model):
-#     FilterMain = models.FileField()
-#     Filter_c = models.FileField()
-#     Filter_h = models.FileField()
-#     Makefile = models.FileField()
-#     cs1300_c = models.FileField()
-#     cs1300_h = models.FileField()
