ddea7ac79967418d199ab8cb622b2782d021bdc0
RSaitta <rsaitta@mail.csuchico.edu>
2019-04-24 22:20:31 -0700

    updated to match models. added softtime exception for revokes
    Former-commit-id: 5e3f901a56d75e2c8f43fd6b34422d9dcdddc1e4

--- web/perfapp/tasks.py
+++ web/perfapp/tasks.py
@@ -6,6 +6,7 @@ django.setup()
 
 from celery import shared_task, task
 from celery.utils.log import get_task_logger
+from celery.exceptions import SoftTimeLimitExceeded
 from celery_progress.backend import ProgressRecorder
 from perfapp.models import Job, Attempt
 from django.contrib.auth.models import User
@@ -33,222 +34,227 @@ def cleanup():
     return "cleanup complete"
 @shared_task(bind=True)
 def dummyTask(self,j_id, uid):
-    print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-    print(j_id)
-    print(uid)
-    user = User.objects.get(id=uid)
-    print(user)
     try:
-        job = Job.objects.get(owner=user, jid=j_id)
-    except Job.DoesNotExist:
-        return 'no matching job'
-    job.status='RUNNING'
-    job.save()
-    progress_recorder = ProgressRecorder(self)
-    for i in range(100):
-        sleep(1)
-        progress_recorder.set_progress(i+1, 100)
-        job.percent_complete=i+1
+        user = User.objects.get(id=uid)
+        try:
+            job = Job.objects.get(owner=user, jid=j_id)
+        except Job.DoesNotExist:
+            return 'no matching job'
+        job.status='RUNNING'
         job.save()
-    job.status='COMPLETE'
-    newAttempt = Attempt(owner=user, note_field=job.note_field, score=79.99, time_stamp=job.time_stamp)
-    newAttempt.save()
-    job.deletable=True
-    job.save()
-    return 'task complete'
+        progress_recorder = ProgressRecorder(self)
+        for i in range(100):
+            sleep(1)
+            progress_recorder.set_progress(i+1, 100)
+            job.percent_complete=i+1
+            job.save()
+        job.status='COMPLETE'
+        progress_recorder.set_progress(100,100)
+        newAttempt = Attempt(owner=user, note_field=job.note_field, score=79.99, time_stamp=job.time_created)
+        newAttempt.save()
+        job.deletable=True
+        job.save()
+        return 'task complete'
+    except SoftTimeLimitExceeded:
+        return 'task aborted'
 
 @shared_task(bind=True)
 def runLab(self,j_id,uid,server,hostname):
-    owner = User.objects.get(id=uid)
-    job = Job.objects.get(owner=user, jid=j_id)
-    job.status='RUNNING'
-    job.save()
-    progress_recorder = ProgressRecorder(self)
-    #current_task.update_state(state='PROGRESS', meta={'current': 0, 'total': 100})
-    toReturn = ""
     try:
-        path = "/perfserv/uploads/"+str(uid)+"/"+str(j_id)
-        #print path
-        config = open(path + "config.txt","r")
-        progress_recorder.set_progress(1, 100)
-        #current_task.update_state(state='PROGRESS', meta={'current': 1, 'total': 100})
-        a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"rm -rf perflab-setup\""
-        b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
-        b.wait()
-        c = b.stdout.read()
-        progress_recorder.set_progress(2, 100)
-        #current_task.update_state(state='PROGRESS', meta={'current': 2, 'total': 100})
-        a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cp -rf perflab-files perflab-setup\""
-        b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
-        b.wait()
-        c = b.stdout.read()
-        progress_recorder.set_progress(3, 100)
-        #current_task.update_state(state='PROGRESS', meta={'current': 3, 'total': 100})
-        f = open(path+"FilterMain.cpp","r")
-        for line in f:
-            if "unistd" in line:
-                return "Illegal Library unistd"
-        a="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "+path+"FilterMain.cpp perfuser@"+server+":~/perflab-setup/"
-        b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
-        b.wait()
-        c = b.stdout.read()
-        progress_recorder.set_progress(4, 100)
-        #current_task.update_state(state='PROGRESS', meta={'current': 4, 'total': 100})
-        for line in config:
-        	#print line
-            line = line.split()
-            if line[1]=="Y":
-                f = open(path+line[0],"r")
-                print (line[0])
-                for line2 in f:
-                    if "unistd" in line2:
-                        return "Illegal Library unistd"
-                a="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "+path+str(line[0])+" perfuser@"+server+":~/perflab-setup/"
-                print (a)
-                b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
-                b.wait()
-                c = b.stdout.read()
-        progress_recorder.set_progress(5, 100)
-        #current_task.update_state(state='PROGRESS', meta={'current': 5, 'total': 100})
-        a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; make filter\""
-        b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
-        b.wait()
-        c = b.stdout.read()
-        e = b.stderr.read()
-        if len(e)>0:
-            if "Error" in e:
-                return e
-            #print e
-            if not "ECDSA" in e:
-                return e
-        #print c
-        progress_recorder.set_progress(10, 100)
-        #current_task.update_state(state='PROGRESS', meta={'current': 10, 'total': 100})
-        status = 10.0
-        tests = 5
-        increment = (100.0-status)/(4.0*float(tests))
-        scores = []
-        #GAUSS
-        gauss = []
-        count = 0
-        while count < tests:
-            a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; ./gauss.sh\""
-            print (a)
-            b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
-            b.wait()
-            c = b.stdout.read()
-            line = c.split()
-            try:
-                print (line)
-                score = float(line[-1])
-                print (score)
-                if not score > 9000 and not score <=0: # Check for and ignore odd scores
-                    scores = scores + [score]
-                    gauss = gauss + [score]
-                    status = status + increment
-                    count = count + 1
-                    progress_recorder.set_progress(status, 100)
-                    #current_task.update_state(state='PROGRESS', meta={'current': status, 'total': 100})
-            except:
-                return "gauss " + str(sys.exc_info()) + " " + hostname + " " + str(c) + "\n"+ str(a) + "\n" + str(b.stderr.read())
-        job.status="SCORING"
+        owner = User.objects.get(id=uid)
+        job = Job.objects.get(owner=user, jid=j_id)
+        job.status='RUNNING'
         job.save()
-        #AVG
-        count = 0
-        avg = []
-        while count < tests:
-            a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; ./avg.sh\""
+        progress_recorder = ProgressRecorder(self)
+        #current_task.update_state(state='PROGRESS', meta={'current': 0, 'total': 100})
+        toReturn = ""
+        try:
+            path = "/perfserv/uploads/"+str(uid)+"/"+str(j_id)
+            #print path
+            config = open(path + "config.txt","r")
+            progress_recorder.set_progress(1, 100)
+            #current_task.update_state(state='PROGRESS', meta={'current': 1, 'total': 100})
+            a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"rm -rf perflab-setup\""
             b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
             b.wait()
             c = b.stdout.read()
-            line = c.split()
-            try:
-                score = float(line[-1])
-                if not score > 9000 and not score <=0: # Check for and ignore odd scores
-                    scores = scores + [score]
-                    avg = avg + [score]
-                    status = status + increment
-                    count = count + 1
-                    #current_task.update_state(state='PROGRESS', meta={'current': status, 'total': 100})
-            except:
-                return "avg " + str(sys.exc_info())+ " " + hostname
-
-        #HLINE
-        count = 0
-        hline = []
-        while count < tests:
-            a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; ./hline.sh\""
+            progress_recorder.set_progress(2, 100)
+            #current_task.update_state(state='PROGRESS', meta={'current': 2, 'total': 100})
+            a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cp -rf perflab-files perflab-setup\""
             b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
             b.wait()
             c = b.stdout.read()
-            line = c.split()
-            try:
-                score = float(line[-1])
-                if not score > 9000 and not score <=0: # Check for and ignore odd scores
-                    scores = scores + [score]
-                    hline = hline + [score]
-                    status = status + increment
-                    count = count + 1
-                    #current_task.update_state(state='PROGRESS', meta={'current': status, 'total': 100})
-            except:
-                return "hline " + str(sys.exc_info()) + " " + hostname
-
-        #EMBOSS
-        count = 0
-        emboss = []
-        while count < tests:
-            a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; ./emboss.sh\""
+            progress_recorder.set_progress(3, 100)
+            #current_task.update_state(state='PROGRESS', meta={'current': 3, 'total': 100})
+            f = open(path+"FilterMain.cpp","r")
+            for line in f:
+                if "unistd" in line:
+                    return "Illegal Library unistd"
+            a="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "+path+"FilterMain.cpp perfuser@"+server+":~/perflab-setup/"
+            b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
+            b.wait()
+            c = b.stdout.read()
+            progress_recorder.set_progress(4, 100)
+            #current_task.update_state(state='PROGRESS', meta={'current': 4, 'total': 100})
+            for line in config:
+            	#print line
+                line = line.split()
+                if line[1]=="Y":
+                    f = open(path+line[0],"r")
+                    print (line[0])
+                    for line2 in f:
+                        if "unistd" in line2:
+                            return "Illegal Library unistd"
+                    a="scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no "+path+str(line[0])+" perfuser@"+server+":~/perflab-setup/"
+                    print (a)
+                    b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
+                    b.wait()
+                    c = b.stdout.read()
+            progress_recorder.set_progress(5, 100)
+            #current_task.update_state(state='PROGRESS', meta={'current': 5, 'total': 100})
+            a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; make filter\""
             b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
             b.wait()
             c = b.stdout.read()
             e = b.stderr.read()
-            line = c.split()
-            #print c
-            try:
-                score = float(line[-1])
-                if not score > 9000 and not score <=0: # Check for and ignore odd scores
-                    scores = scores + [score]
-                    emboss = emboss + [score]
-                    status = status + increment
-                    count = count + 1
-                    #current_task.update_state(state='PROGRESS', meta={'current': status, 'total': 100})
-            except:
+            if len(e)>0:
+                if "Error" in e:
+                    return e
                 #print e
-                return  "emboss " + str(sys.exc_info()) + " " + hostname
+                if not "ECDSA" in e:
+                    return e
+            #print c
+            progress_recorder.set_progress(10, 100)
+            #current_task.update_state(state='PROGRESS', meta={'current': 10, 'total': 100})
+            status = 10.0
+            tests = 5
+            increment = (100.0-status)/(4.0*float(tests))
+            scores = []
+            #GAUSS
+            gauss = []
+            count = 0
+            while count < tests:
+                a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; ./gauss.sh\""
+                print (a)
+                b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
+                b.wait()
+                c = b.stdout.read()
+                line = c.split()
+                try:
+                    print (line)
+                    score = float(line[-1])
+                    print (score)
+                    if not score > 9000 and not score <=0: # Check for and ignore odd scores
+                        scores = scores + [score]
+                        gauss = gauss + [score]
+                        status = status + increment
+                        count = count + 1
+                        progress_recorder.set_progress(status, 100)
+                        #current_task.update_state(state='PROGRESS', meta={'current': status, 'total': 100})
+                except:
+                    return "gauss " + str(sys.exc_info()) + " " + hostname + " " + str(c) + "\n"+ str(a) + "\n" + str(b.stderr.read())
+            job.status="SCORING"
+            job.save()
+            #AVG
+            count = 0
+            avg = []
+            while count < tests:
+                a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; ./avg.sh\""
+                b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
+                b.wait()
+                c = b.stdout.read()
+                line = c.split()
+                try:
+                    score = float(line[-1])
+                    if not score > 9000 and not score <=0: # Check for and ignore odd scores
+                        scores = scores + [score]
+                        avg = avg + [score]
+                        status = status + increment
+                        count = count + 1
+                        #current_task.update_state(state='PROGRESS', meta={'current': status, 'total': 100})
+                except:
+                    return "avg " + str(sys.exc_info())+ " " + hostname
 
-        scores.sort()
-        #print scores
+            #HLINE
+            count = 0
+            hline = []
+            while count < tests:
+                a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; ./hline.sh\""
+                b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
+                b.wait()
+                c = b.stdout.read()
+                line = c.split()
+                try:
+                    score = float(line[-1])
+                    if not score > 9000 and not score <=0: # Check for and ignore odd scores
+                        scores = scores + [score]
+                        hline = hline + [score]
+                        status = status + increment
+                        count = count + 1
+                        #current_task.update_state(state='PROGRESS', meta={'current': status, 'total': 100})
+                except:
+                    return "hline " + str(sys.exc_info()) + " " + hostname
+
+            #EMBOSS
+            count = 0
+            emboss = []
+            while count < tests:
+                a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"cd perflab-setup/ ; ./emboss.sh\""
+                b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
+                b.wait()
+                c = b.stdout.read()
+                e = b.stderr.read()
+                line = c.split()
+                #print c
+                try:
+                    score = float(line[-1])
+                    if not score > 9000 and not score <=0: # Check for and ignore odd scores
+                        scores = scores + [score]
+                        emboss = emboss + [score]
+                        status = status + increment
+                        count = count + 1
+                        #current_task.update_state(state='PROGRESS', meta={'current': status, 'total': 100})
+                except:
+                    #print e
+                    return  "emboss " + str(sys.exc_info()) + " " + hostname
+
+            scores.sort()
+            #print scores
 
 
 
-        toReturn += "gauss: "
-        for g in gauss:
-            toReturn += str(g) + ".. "
-        toReturn += "\navg: "
-        for a in avg:
-            toReturn += str(a) + ".. "
-        toReturn += "\nhline: "
-        for h in hline:
-            toReturn += str(h) + ".. "
-        toReturn += "\nemboss: "
-        for e in emboss:
-            toReturn += str(e) + ".. "
-        toReturn += "\nScores are "
-        for s in scores:
-            toReturn += str(int(s)) + " "
-        cpe = scores[int((len(scores)+1)/2)]
-        toReturn += "\nmedian CPE is " + str(int(cpe)) + " "
-        if cpe > 4000:
-            score = 0
-        else:
-            #score = math.log(6000-cpe) * 46.93012749-305.91731341
-            score = 119.653*math.exp(-0.001196*cpe)
-            if score > 110:
-                score = 110
-        score = int(score)
-        toReturn +="\nResulting score is " + str(score) + "\n"
-    except:
-        toReturn = "Unexpected error: " + str(sys.exc_info())
-    newAttempt = Attempt(owner=user, note_field=job.note_field, score=79.99, time_stamp=job.time_stamp)
-    newAttempt.save()
-    return toReturn
+            toReturn += "gauss: "
+            for g in gauss:
+                toReturn += str(g) + ".. "
+            toReturn += "\navg: "
+            for a in avg:
+                toReturn += str(a) + ".. "
+            toReturn += "\nhline: "
+            for h in hline:
+                toReturn += str(h) + ".. "
+            toReturn += "\nemboss: "
+            for e in emboss:
+                toReturn += str(e) + ".. "
+            toReturn += "\nScores are "
+            for s in scores:
+                toReturn += str(int(s)) + " "
+            cpe = scores[int((len(scores)+1)/2)]
+            toReturn += "\nmedian CPE is " + str(int(cpe)) + " "
+            if cpe > 4000:
+                score = 0
+            else:
+                #score = math.log(6000-cpe) * 46.93012749-305.91731341
+                score = 119.653*math.exp(-0.001196*cpe)
+                if score > 110:
+                    score = 110
+            score = int(score)
+            toReturn +="\nResulting score is " + str(score) + "\n"
+        except:
+            toReturn = "Unexpected error: " + str(sys.exc_info())
+        newAttempt = Attempt(owner=user, note_field=job.note_field, score=79.99, time_stamp=job.time_stamp)
+        newAttempt.save()
+        return toReturn
+    except SoftTimeLimitExceeded:
+        b.kill()
+        a="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no perfuser@"+server+ " \"^C;\""
+        return "Task Stopped by user"
