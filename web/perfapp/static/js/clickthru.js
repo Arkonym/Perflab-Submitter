4d3f72de79fc3a6034e8c74a9655b53f44d8d2ea
RSaitta <rsaitta@mail.csuchico.edu>
2019-04-26 14:24:34 -0700

    profile job table now clicks through to progress;progress redirects to profile when done
    Former-commit-id: f7aba8ab40c435d89919be51f53e2e4e22aad990

--- web/perfapp/static/js/clickthru.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $('#clickable tr').click(function(){
-    var n_href = $(this).find("a").attr("href")
-    if (n_href) {
-      window.location.href = href
-    }
-  })
-});
