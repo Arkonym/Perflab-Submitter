33c37908acf44421789fadcef57fe4d81ac071c7
RSaitta <rsaitta@mail.csuchico.edu>
2019-05-08 23:56:24 -0700

    moved server lease check to init function
    Former-commit-id: 620764d2e8eac309307e120dea8b8e9fcfaf86c6

--- web/perfapp/management/commands/init_servers.py
+++ web/perfapp/management/commands/init_servers.py
@@ -2,7 +2,9 @@ from django.core.management.base import BaseCommand
 from perfapp.models import Server
 
 import os
-
+from subprocess import Popen, PIPE
+from redis import Redis
+red = Redis(host='redis', port=6379)
 
 class Command(BaseCommand):
     help = 'Run command to initialize all servers in lease file.'
@@ -21,10 +23,29 @@ class Command(BaseCommand):
         Server(ip = '192.168.1.28', hostname = 'rpi18').save()
         Server(ip = '192.168.1.34', hostname = 'rpi24').save()
 
+    def _check(self):
+        print("Server lease check")
+        red.set('servers',0)
+        try:
+            servers = Server.objects.all()
+            for serv in servers:
+                a="ping -c 1"+ str(serv.ip) + "| grep \"1 received\""
+                b=Popen(a, shell=True, stdout=PIPE, stderr=PIPE)
+                b.wait()
+                c = b.stdout.read().decode()
+                if c == None:
+                    serv.online= False
+                else: red.incr('servers')
+
+            # return "Server lease init completed"
+        except:
+            return "Server init error. Ensure hardware available on network"
+
 
     def handle(self, *args, **options):
         try:
             self._init()
+            self._check()
         except:
             pass
         print("Server list initialized")
